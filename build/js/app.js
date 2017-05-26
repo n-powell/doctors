112fb55cbd03b7bcc8c9f06b8b267a783ae939e1
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-26 16:44:59 -0700

    add semicolon

--- build/js/app.js
+++ build/js/app.js
@@ -25,7 +25,7 @@ exports.docModule = Doc;
 var Doc = require('./../js/doctor.js').docModule;
 
 var displayDoctors = function (doctors) {
-  $('#showDoctors').text("")
+  $('#showDoctors').text("");
   doctors.data.forEach(function(doctor){
       $('#showDoctors').append('<li>'+ '<img src=' + doctor.profile.image_url + '>' + doctor.profile.first_name + ' ' + doctor.profile.last_name + '<br>' +  doctor.profile.bio + '<br>' + '<a href=\"' + doctor.practices[0].website + '\">Visit my website</a>' + '<br>' + doctor.practices[0].phones[0].number + '<br>' + '</li>' + '<br>' + '<hr>'+ '<br>');
   });
